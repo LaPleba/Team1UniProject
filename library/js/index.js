0f5f590e67afd52723ac24053226ded112685193
Alex19998282 <36903262+Alex19998282@users.noreply.github.com>
2018-10-22 22:40:16 +0100

    Reworked file structure and started refactoring code
    Reworked the file structure to something more sensible and began refactoring some code, notably moving passes to a separate js file. Also began migration to typescript

---
+++ library/js/index.js
@@ -0,0 +1,14 @@
+//This stuff is in a seperate file because exposing passwords is dumb
+$('#form').on('submit', (function (e) {
+    e.preventDefault();
+    var username = $('#usernamefield').val();
+    var password = $('#passwordfield').val();
+    if (username === "admin") {
+        if (password === "Admin123") {
+            window.location.replace("library/html/main.html");
+        }
+    }
+    else {
+        $('#failedlogin').show();
+    }
+}));
